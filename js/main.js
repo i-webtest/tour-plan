858355e52985c375f3b1ce61fbbf6bd974991e7e
i-webtest <stasbass80@gmail.com>
2021-11-22 21:14:25 +0300

    create: switch for slider using keyboard buttons

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
 
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
 });
+
