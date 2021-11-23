08e1b42f1e030715bd1cd7ac726f945b21746b0b
i-webtest <stasbass80@gmail.com>
2021-11-23 22:01:36 +0300

    create: added yandex maps

--- js/main.js
+++ js/main.js
@@ -7,10 +7,27 @@ const swiper = new Swiper('.swiper', {
     onlyInViewport: false,
   },
 
+  
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+
 });
 
+
+ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [7.89074526, 98.29469841],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 17
+        });
+    }
