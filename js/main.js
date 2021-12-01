3425cfe8a1f47f07852b999fdf75b7ebabadc490
i-webtest <stasbass80@gmail.com>
2021-12-01 14:31:40 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -38,3 +38,8 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 
 
 
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  console.log("клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
