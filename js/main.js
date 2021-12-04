a4abd77fbe5e58824d7e8727d6600746f321036e
i-webtest <stasbass80@gmail.com>
2021-12-04 15:59:56 +0300

    create: close modal window esc

--- js/main.js
+++ js/main.js
@@ -54,7 +54,6 @@ $(document).ready(function () {
   var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
-
   
 
   function openModal() {
@@ -63,22 +62,26 @@ $(document).ready(function () {
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
   };
+
+  
+
   function closeModal(event) {
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
-
     
   };
+  // Закрытие окна по кнопке ESC
+  $(document).keydown(function (e) { 
+    if (e.keyCode == 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
   
 
-  // $(this).keydown(function(eventObject){
-  //     if (eventObject.which == 27)
-  //         $('.modal').hide();
-  // });
-
-
 });
-
